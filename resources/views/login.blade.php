c08e6bf3f40fbd21853645a31635660e9cdc7d31
Ruth <meldabutet04@gmail.com>
2024-10-14 06:20:47 +0700

    add login page layout

---
+++ resources/views/login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Login</title>
+ <link rel="stylesheet"
+href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+ <div class="container">
+ <h1 class="text-center mt-5">Login Page</h1>
+ <form>
+ <div class="form-group">
+ <label for="email">Email address</label>
+ <input type="email" class="form-control" id="email" placeholder="Enter your email">
+ </div>
+ <div class="form-group">
+ <label for="password">Password</label>
+ <input type="password" class="form-control" id="password" placeholder="Enter your
+password">
+ </div>
+ <button type="submit" class="btn btn-primary">Login</button>
+ </form>
+ </div>
+</body>
+</html>
